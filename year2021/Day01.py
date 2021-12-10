c1b908b2d20d1f808c41fef0e9f50b05835d8d90
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-10 21:32:02 +0000

    day 1

---
+++ year2021/Day01.py
@@ -0,0 +1,13 @@
+file = open("./data/year2021/day01.txt", "r")
+depth = [int(line.rstrip('\n')) for line in file]
+n = len(depth)
+
+a, b = 0, 0
+for i in range(1, n):
+    if depth[i] > depth[i - 1]:
+        a += 1
+for i in range(3, n):
+    if depth[i] > depth[i - 3]:
+        b += 1
+print(a)
+print(b)
