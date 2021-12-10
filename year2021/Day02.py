23fbb9522988e6e09ac775a480cf93d5ab0aebb2
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-10 21:45:57 +0000

    day 2

---
+++ year2021/Day02.py
@@ -0,0 +1,18 @@
+file = open("./data/year2021/day02.txt", "r")
+lines = [line.rstrip('\n').split(" ") for line in file]
+
+h1, d1 = 0, 0
+h2, d2, a2 = 0, 0, 0
+for line in lines:
+    if line[0] == "forward":
+        h1 += int(line[1])
+        h2 += int(line[1])
+        d2 += a2 * int(line[1])
+    elif line[0] == "down":
+        d1 += int(line[1])
+        a2 += int(line[1])
+    elif line[0] == "up":
+        d1 -= int(line[1])
+        a2 -= int(line[1])
+print(h1 * d1)
+print(h2 * d2)
