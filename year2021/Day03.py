e5e9f6acf3ee508878c6e81fecb05e4ec40113ad
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-11 09:55:42 +0000

    day 3

---
+++ year2021/Day03.py
@@ -0,0 +1,32 @@
+file = open("./data/year2021/day03.txt", "r")
+lines = [line.rstrip('\n').split(" ") for line in file]
+bin_nums = [n for line in lines for n in line]
+n_bits = len(bin_nums[0])
+
+
+def get_bit(bin_nums, pos, is_max):
+    bits = [n[pos] for n in bin_nums]
+    c = bits.count("1")
+    return ("1" if 2 * c >= len(bits) else "0") if is_max != 0 else ("1" if 2 * c < len(bits) else "0")
+
+
+def filter_bin_nums(bin_nums, pos, is_max):
+    if len(bin_nums) == 1:
+        return bin_nums
+    b = get_bit(bin_nums, pos, is_max)
+    return list(filter(lambda x: x[pos] == b, bin_nums))
+
+
+gamma, epsilon = "", ""
+for pos in range(n_bits):
+    gamma += get_bit(bin_nums, pos, 1)
+    epsilon += get_bit(bin_nums, pos, 0)
+power = int(gamma, 2) * int(epsilon, 2)
+print(power)
+
+ox, co2 = bin_nums, bin_nums
+for pos in range(n_bits):
+    ox = filter_bin_nums(ox, pos, 1)
+    co2 = filter_bin_nums(co2, pos, 0)
+support = int(ox[0], 2) * int(co2[0], 2)
+print(support)
