26acbac046d88426147d37d68ac4aca293faaf02
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-11 04:22:40 +0000

    Create Day06.py

---
+++ year2021/Day06.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+file = open("./data/year2021/day06.txt", "r")
+lines = [line.rstrip('\n') for line in file]
+fish = [int(f) for f in lines[0].split(",")]
+
+c = np.zeros((257, 9), dtype=int)
+
+for i in range(len(fish)):
+    c[0][fish[i]] += 1
+for d in range(1, 257):
+    for i in range(0, 8):
+        c[d][i] = c[d - 1][i + 1]
+    c[d][8] = c[d - 1][0]
+    c[d][6] += c[d - 1][0]
+print(sum(c[80]))
+print(sum(c[256]))
