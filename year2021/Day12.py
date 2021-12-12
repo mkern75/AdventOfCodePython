2590ded03552be74a6a530c49d0a38f246e5f9f5
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-12 07:34:03 +0000

    some more day 12 optimisation

--- year2021/Day12.py
+++ year2021/Day12.py
@@ -4,19 +4,15 @@ file = open("./data/year2021/day12.txt", "r")
 lines = [line.rstrip('\n') for line in file]
 
 
-def count_paths(path, conn, allow_twice):
+def count_paths(path, conn, allow_twice=False):
     if path[-1] == "end":
         return 1
     cnt = 0
     for neighbour in conn[path[-1]]:
         if neighbour.isupper() or neighbour not in path:
-            path.append(neighbour)
-            cnt += count_paths(path, conn, allow_twice)
-            path.pop()
+            cnt += count_paths(path + [neighbour], conn, allow_twice)
         elif allow_twice and neighbour not in ["start", "end"]:
-            path.append(neighbour)
-            cnt += count_paths(path, conn, False)
-            path.pop()
+            cnt += count_paths(path + [neighbour], conn, False)
     return cnt
 
 
@@ -25,5 +21,5 @@ for line in lines:
     cave1, cave2 = line.split("-")
     conn[cave1].append(cave2)
     conn[cave2].append(cave1)
-print(count_paths(["start"], conn, False))
+print(count_paths(["start"], conn))
 print(count_paths(["start"], conn, True))
