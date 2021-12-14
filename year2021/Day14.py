83a2917d632a2896baaead7a156a179f83a39c99
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-14 08:09:37 +0000

    2021 day 14: learning about counters

--- year2021/Day14.py
+++ year2021/Day14.py
@@ -1,27 +1,27 @@
-from collections import defaultdict
+from collections import Counter
 
 file = open("./year2021/data/day14.txt", "r")
 lines = [line.rstrip('\n') for line in file]
 
-RULES = defaultdict(str)
+R = {}  # dict of the rules
 for i in range(2, len(lines)):
-    fr, to = lines[i].split(" -> ")
-    RULES[fr] = to
+    pair, element = lines[i].split(" -> ")
+    R[pair] = element
 
-template = lines[0]
-PC = defaultdict(int)  # dict: element pair to its count
+template = lines[0]  # initial polymer template
+CP = Counter()  # counter of pairs
 for i in range(len(template) - 1):
-    PC[template[i:i + 2]] += 1
+    CP[template[i:i + 2]] += 1
 
 for step in range(1, 41):
-    NPC = defaultdict(int)  # new dict for new step
-    for pair in PC:
-        NPC[pair[0] + RULES[pair]] += PC[pair]
-        NPC[RULES[pair] + pair[1]] += PC[pair]
-    PC = NPC
+    NCP = Counter()  # new counter of pairs for new step
+    for pair in CP:
+        NCP[pair[0] + R[pair]] += CP[pair]
+        NCP[R[pair] + pair[1]] += CP[pair]
+    CP = NCP
     if step in [10, 40]:
-        EC = defaultdict(int)  # dict: element to its count
-        for pair in PC:
-            EC[pair[0]] += PC[pair]
-        EC[template[-1]] += 1  # don't forget the very last element
-        print(max(EC.values()) - min(EC.values()))
+        CE = Counter()  # counter of elements
+        for pair in CP:
+            CE[pair[0]] += CP[pair]
+        CE[template[-1]] += 1  # don't forget the very last element
+        print(max(CE.values()) - min(CE.values()))
