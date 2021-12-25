70b1814056317826a92b94cf4c202b164dc598b3
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-25 06:41:21 +0000

    final final change - Merry Christmas!

--- year2021/Day25.py
+++ year2021/Day25.py
@@ -1,40 +1,32 @@
-from datetime import datetime
-
 file = open("./year2021/data/day25.txt", "r")
 lines = [line.rstrip('\n') for line in file]
+G = [[c for c in line] for line in lines]
 
 
 def one_step(g):
-    R, C, move = len(lines), len(lines[0]), False
-    ng = [[0] * C for _ in range(R)]
+    R, C, move = len(g), len(g[0]), False
+    ng = [["."] * C for _ in range(R)]
     for r in range(R):
         for c in range(C):
-            if g[r][c] == 1:
-                if g[r][(c + 1) % C] == 0:
-                    ng[r][(c + 1) % C] = 1
+            if g[r][c] == ">":
+                if g[r][(c + 1) % C] == ".":
+                    ng[r][(c + 1) % C] = ">"
                     move = True
                 else:
-                    ng[r][c] = 1
+                    ng[r][c] = ">"
     for r in range(R):
         for c in range(C):
-            if g[r][c] == 2:
-                if g[(r + 1) % R][c] != 2 and ng[(r + 1) % R][c] == 0:
-                    ng[(r + 1) % R][c] = 2
+            if g[r][c] == "v":
+                if g[(r + 1) % R][c] != "v" and ng[(r + 1) % R][c] == ".":
+                    ng[(r + 1) % R][c] = "v"
                     move = True
                 else:
-                    ng[r][c] = 2
+                    ng[r][c] = "v"
     return move, ng
 
-
-print("start :", datetime.now().strftime("%H:%M:%S.%f"))
-
-m = {".": 0, ">": 1, "v": 2}
-G = [[m[c] for c in line] for line in lines]
 
 step, move = 0, True
 while move:
     step += 1
     move, G = one_step(G)
 print("part 1:", step)
-
-print("finish:", datetime.now().strftime("%H:%M:%S.%f"))
