583ed6627dad3c4d5a4a3e1c10fdce3858a98b91
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-25 05:38:35 +0000

    over and out for AoC 2021

---
+++ year2021/Day25.py
@@ -0,0 +1,41 @@
+import numpy as np
+from datetime import datetime
+
+file = open("./year2021/data/day25.txt", "r")
+lines = [line.rstrip('\n') for line in file]
+
+
+def one_step(g):
+    R, C, move = len(lines), len(lines[0]), False
+    ng = np.zeros((R, C), dtype=int)
+    for r in range(R):
+        for c in range(C):
+            if g[r][c] == 1:
+                if g[r][(c + 1) % C] == 0:
+                    ng[r][(c + 1) % C] = 1
+                    move = True
+                else:
+                    ng[r][c] = 1
+    for r in range(R):
+        for c in range(C):
+            if g[r][c] == 2:
+                if g[(r + 1) % R][c] != 2 and ng[(r + 1) % R][c] == 0:
+                    ng[(r + 1) % R][c] = 2
+                    move = True
+                else:
+                    ng[r][c] = 2
+    return move, ng
+
+
+print("start :", datetime.now().strftime("%H:%M:%S.%f"))
+
+m = {".": 0, ">": 1, "v": 2}
+G = np.array([[m[c] for c in line] for line in lines])
+
+step, move = 0, True
+while move:
+    step += 1
+    move, G = one_step(G)
+print("part 1:", step)
+
+print("finish:", datetime.now().strftime("%H:%M:%S.%f"))
