935078a9cb9496e31cb10ebd06092bb2fe77a70a
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-26 06:22:06 +0000

    removal of match statement

--- year2019/Day05.py
+++ year2019/Day05.py
@@ -24,44 +24,45 @@ def run(program, inp):
     ip, inpp = 0, 0
     while ip in mem:
         opcode, pmode = mem[ip] % 100, mem[ip] // 100
-        match opcode:
-            case 1:  # add
-                mem[mem[ip + 3]] = get_value(pmode, 1, ip + 1, mem) + get_value(pmode, 2, ip + 2, mem)
-                ip += 4
-            case 2:  # multiply
-                mem[mem[ip + 3]] = get_value(pmode, 1, ip + 1, mem) * get_value(pmode, 2, ip + 2, mem)
-                ip += 4
-            case 3:  # input
-                mem[mem[ip + 1]] = inp[inpp]
-                inpp += 1
-                ip += 2
-            case 4:  # output
-                outp += [get_value(pmode, 1, ip + 1, mem)]
-                ip += 2
-            case 5:  # jump-if-true
-                if get_value(pmode, 1, ip + 1, mem) != 0:
-                    ip = get_value(pmode, 2, ip + 2, mem)
-                else:
-                    ip += 3
-            case 6:  # jump-if-false
-                if get_value(pmode, 1, ip + 1, mem) == 0:
-                    ip = get_value(pmode, 2, ip + 2, mem)
-                else:
-                    ip += 3
-            case 7:  # less than
-                if get_value(pmode, 1, ip + 1, mem) < get_value(pmode, 2, ip + 2, mem):
-                    mem[mem[ip + 3]] = 1
-                else:
-                    mem[mem[ip + 3]] = 0
-                ip += 4
-            case 8:  # equals
-                if get_value(pmode, 1, ip + 1, mem) == get_value(pmode, 2, ip + 2, mem):
-                    mem[mem[ip + 3]] = 1
-                else:
-                    mem[mem[ip + 3]] = 0
-                ip += 4
-            case 99:
-                break
+        if opcode == 1:  # add
+            mem[mem[ip + 3]] = get_value(pmode, 1, ip + 1, mem) + get_value(pmode, 2, ip + 2, mem)
+            ip += 4
+        elif opcode == 2:  # multiply
+            mem[mem[ip + 3]] = get_value(pmode, 1, ip + 1, mem) * get_value(pmode, 2, ip + 2, mem)
+            ip += 4
+        elif opcode == 3:  # input
+            mem[mem[ip + 1]] = inp[inpp]
+            inpp += 1
+            ip += 2
+        elif opcode == 4:  # output
+            outp += [get_value(pmode, 1, ip + 1, mem)]
+            ip += 2
+        elif opcode == 5:  # jump-if-true
+            if get_value(pmode, 1, ip + 1, mem) != 0:
+                ip = get_value(pmode, 2, ip + 2, mem)
+            else:
+                ip += 3
+        elif opcode == 6:  # jump-if-false
+            if get_value(pmode, 1, ip + 1, mem) == 0:
+                ip = get_value(pmode, 2, ip + 2, mem)
+            else:
+                ip += 3
+        elif opcode == 7:  # less than
+            if get_value(pmode, 1, ip + 1, mem) < get_value(pmode, 2, ip + 2, mem):
+                mem[mem[ip + 3]] = 1
+            else:
+                mem[mem[ip + 3]] = 0
+            ip += 4
+        elif opcode == 8:  # equals
+            if get_value(pmode, 1, ip + 1, mem) == get_value(pmode, 2, ip + 2, mem):
+                mem[mem[ip + 3]] = 1
+            else:
+                mem[mem[ip + 3]] = 0
+            ip += 4
+        elif opcode == 99:
+            break
+        else:
+            assert False, "wrong opcode"
     return mem, outp
 
 
@@ -71,4 +72,3 @@ print("part 1:", outp[-1])
 
 mem, outp = run(program, [5])
 print("part 2:", outp[-1])
-
