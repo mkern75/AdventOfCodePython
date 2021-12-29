1b7c761829e070680b7d885ba935d9adad8fbb7e
mkern75 <82466426+mkern75@users.noreply.github.com>
2021-12-29 09:47:58 +0000

    Update MonorailComputer.py

--- year2016/MonorailComputer.py
+++ year2016/MonorailComputer.py
@@ -1,37 +1,51 @@
 class MonorailComputer:
     def __init__(self, program):
-        self.program = program.copy()
+        self.program = [instr.split() for instr in program]
         self.memory = {"a": 0, "b": 0, "c": 0, "d": 0}
         self.ipp = 0  # instruction pointer
 
     def reset(self, program):
-        self.program = program.copy()
+        self.program = [instr.split() for instr in program]
         self.memory = {"a": 0, "b": 0, "c": 0, "d": 0}
         self.ipp = 0  # instruction pointer
 
+    def val(self, s):
+        if s in ["a", "b", "c", "d"]:
+            return self.memory[s]
+        else:
+            return int(s)
+
     def run(self):
         while 0 <= self.ipp < len(self.program):
-            s = self.program[self.ipp].split()
-            if s[0] == "cpy":
-                if s[1] in ["a", "b", "c", "d"]:
-                    self.memory[s[2]] = self.memory[s[1]]
-                else:
-                    self.memory[s[2]] = int(s[1])
+            self.run_one_instruction()
+
+    def run_one_instruction(self):
+        instr = self.program[self.ipp]
+        if instr[0] == "cpy":
+            self.memory[instr[2]] = self.val(instr[1])
+            self.ipp += 1
+        elif instr[0] == "inc":
+            self.memory[instr[1]] += 1
+            self.ipp += 1
+        elif instr[0] == "dec":
+            self.memory[instr[1]] -= 1
+            self.ipp += 1
+        elif instr[0] == "jnz":
+            if self.val(instr[1]) != 0:
+                self.ipp += self.val(instr[2])
+            else:
                 self.ipp += 1
-            elif s[0] == "inc":
-                self.memory[s[1]] += 1
-                self.ipp += 1
-            elif s[0] == "dec":
-                self.memory[s[1]] -= 1
-                self.ipp += 1
-            elif s[0] == "jnz":
-                if s[1] in ["a", "b", "c", "d"]:
-                    if self.memory[s[1]] != 0:
-                        self.ipp += int(s[2])
+        elif instr[0] == "tgl":
+            addr = self.ipp + self.val(instr[1])
+            if 0 <= addr < len(self.program):
+                if len(self.program[addr]) == 2:
+                    if self.program[addr][0] == "inc":
+                        self.program[addr][0] = "dec"
                     else:
-                        self.ipp += 1
-                else:
-                    if int(s[1]) != 0:
-                        self.ipp += int(s[2])
+                        self.program[addr][0] = "inc"
+                elif len(self.program[addr]) == 3:
+                    if self.program[addr][0] == "jnz":
+                        self.program[addr][0] = "cpy"
                     else:
-                        self.ipp += 1
+                        self.program[addr][0] = "jnz"
+            self.ipp += 1
