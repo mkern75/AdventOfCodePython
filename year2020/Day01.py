61dced63ff11d4a3cbf5a017f46fa0140ae28124
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-01-17 17:41:42 +0000

    2020/move to utils functionality

--- year2020/Day01.py
+++ year2020/Day01.py
@@ -1,22 +1,16 @@
-import time
+from utils import load_numbers
+from itertools import combinations
 
-t0 = time.time()
 INPUT_FILE = "./year2020/data/day01.txt"
 
-file = open(INPUT_FILE, "r")
-lines = [line.rstrip('\n') for line in file]
-n = [int(line) for line in lines]
+n = load_numbers(INPUT_FILE)
 
-for i in range(len(n) - 1):
-    for j in range(i + 1, len(n)):
-        if n[i] + n[j] == 2020:
-            ans1 = n[i] * n[j]
-print("part 1:", ans1, f"  ({time.time() - t0:.3f}s)")
-t1 = time.time()
+for c in combinations(n, 2):
+    if c[0] + c[1] == 2020:
+        ans1 = c[0] * c[1]
+print("part 1:", ans1)
 
-for i in range(len(n) - 2):
-    for j in range(i + 1, len(n) - 1):
-        for k in range(j + 1, len(n)):
-            if n[i] + n[j] + n[k] == 2020:
-                ans2 = n[i] * n[j] * n[k]
-print("part 2:", ans2, f"  ({time.time() - t1:.3f}s)")
+for c in combinations(n, 3):
+    if c[0] + c[1] + c[2] == 2020:
+        ans2 = c[0] * c[1] * c[2]
+print("part 2:", ans2)
--- year2020/Day02.py
+++ year2020/Day02.py
@@ -1,25 +1,24 @@
+from utils import load_lines
+from collections import namedtuple
 import re
-import time
 
-t0 = time.time()
 INPUT_FILE = "./year2020/data/day02.txt"
+Rule = namedtuple("password_rule", "low high letter password")
 
-file = open(INPUT_FILE, "r")
-lines = [line.rstrip('\n') for line in file]
+rules = []
+for line in load_lines(INPUT_FILE):
+    m = re.compile(r"([0-9]+)-([0-9]+) ([a-z]+): (.*)").match(line)
+    rules += [Rule(int(m.group(1)), int(m.group(2)), m.group(3), m.group(4))]
 
 ans1 = 0
-for line in lines:
-    m = re.compile(r"([0-9]+)-([0-9]+) ([a-z]+): (.*)").match(line)
-    low, high, c, pw = int(m.group(1)), int(m.group(2)), m.group(3), m.group(4)
-    if low <= pw.count(c) <= high:
+for rule in rules:
+    if rule.low <= rule.password.count(rule.letter) <= rule.high:
         ans1 += 1
-print("part 1:", ans1, f"  ({time.time() - t0:.3f}s)")
-t1 = time.time()
+print("part 1:", ans1)
 
 ans2 = 0
-for line in lines:
-    m = re.compile(r"([0-9]+)-([0-9]+) ([a-z]+): (.*)").match(line)
-    low, high, c, pw = int(m.group(1)), int(m.group(2)), m.group(3), m.group(4)
-    if (pw[low - 1] == c and pw[high - 1] != c) or (pw[low - 1] != c and pw[high - 1] == c):
+for rule in rules:
+    if (rule.password[rule.low - 1] == rule.letter and rule.password[rule.high - 1] != rule.letter) or (
+            rule.password[rule.low - 1] != rule.letter and rule.password[rule.high - 1] == rule.letter):
         ans2 += 1
-print("part 2:", ans2, f"  ({time.time() - t1:.3f}s)")
+print("part 2:", ans2)
