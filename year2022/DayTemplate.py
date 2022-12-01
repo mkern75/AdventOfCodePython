f7f6e28e9bd514f510e1422158759bc29c043e6a
mkern75 <82466426+mkern75@users.noreply.github.com>
2022-12-01 05:39:36 +0000

    Create DayTemplate.py

---
+++ year2022/DayTemplate.py
@@ -0,0 +1,12 @@
+import utils
+
+INPUT_FILE = "./year2022/data/day02test.txt"
+# INPUT_FILE = "./year2022/data/day02.txt"
+
+data = utils.load_lines(INPUT_FILE)
+
+ans1 = 0
+ans2 = 0
+
+print(f"part 1: {ans1}")
+print(f"part 2: {ans2}")
