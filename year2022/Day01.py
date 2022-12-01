bdee997f304da7b764b93a0e02b2bc9fdff6ba40
mkern75 <82466426+mkern75@users.noreply.github.com>
2022-12-01 05:30:58 +0000

    Update Day01.py

--- year2022/Day01.py
+++ year2022/Day01.py
@@ -10,6 +10,8 @@ for cal in [line.rstrip('\n') for line in open(INPUT_FILE, "r")]:
 elves += [elf]
 
 elves.sort(reverse=True)
+ans1 = elves[0]
+ans2 = sum(elves[:3])
 
-print(f"part 1: {elves[0]}")
-print(f"part 2: {sum(elves[:3])}")
+print(f"part 1: {ans1}")
+print(f"part 2: {ans2}")
