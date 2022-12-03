e9054b8f4a870b789ba0b1437409a03351c116cd
mkern75 <82466426+mkern75@users.noreply.github.com>
2022-12-03 05:29:18 +0000

    2022 Day 3

---
+++ year2022/Day03.py
@@ -0,0 +1,15 @@
+INPUT_FILE = "./year2022/data/day03.txt"
+rucksacks = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
+
+ans1 = 0
+for rucksack in rucksacks:
+    n = len(rucksack) // 2
+    item = set(rucksack[:n]).intersection(rucksack[n:]).pop()
+    ans1 += ord(item) + (1 - ord("a") if item.islower() else 27 - ord("A"))
+print(f"part 1: {ans1}")
+
+ans2 = 0
+for i in range(0, len(rucksacks), 3):
+    item = set(rucksacks[i]).intersection(rucksacks[i + 1]).intersection(rucksacks[i + 2]).pop()
+    ans2 += ord(item) + (1 - ord("a") if item.islower() else 27 - ord("A"))
+print(f"part 2: {ans2}")
