3147b31550d807dbf7105cc3014ff1a9d9afb224
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-12-03 19:36:06 +0000

    improvement how to retrieve single item from set

--- year2022/Day03.py
+++ year2022/Day03.py
@@ -4,12 +4,12 @@ rucksacks = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
 ans1 = 0
 for rucksack in rucksacks:
     n = len(rucksack) // 2
-    item = (set(rucksack[:n]) & set(rucksack[n:])).pop()
+    item, = set(rucksack[:n]) & set(rucksack[n:])
     ans1 += ord(item) + (1 - ord("a") if item.islower() else 27 - ord("A"))
 print(f"part 1: {ans1}")
 
 ans2 = 0
 for i in range(0, len(rucksacks), 3):
-    item = (set(rucksacks[i]) & set(rucksacks[i + 1]) & set(rucksacks[i + 2])).pop()
+    item, = set(rucksacks[i]) & set(rucksacks[i + 1]) & set(rucksacks[i + 2])
     ans2 += ord(item) + (1 - ord("a") if item.islower() else 27 - ord("A"))
 print(f"part 2: {ans2}")
