06aae378b37258c7b51a11ae3f7f882d6c61e009
mkern75 <82466426+mkern75@users.noreply.github.com>
2022-12-03 05:50:27 +0000

    minor updates to template file

--- year2022/DayTemplate.py
+++ year2022/DayTemplate.py
@@ -1,12 +1,13 @@
 import utils
 
-INPUT_FILE = "./year2022/data/day02test.txt"
-# INPUT_FILE = "./year2022/data/day02.txt"
-
+INPUT_FILE = "./year2022/data/day04test.txt"
+# INPUT_FILE = "./year2022/data/day04.txt"
 data = utils.load_lines(INPUT_FILE)
 
-ans1 = 0
-ans2 = 0
+ans1, ans2 = 0, 0
+
+for line in data:
+    pass
 
 print(f"part 1: {ans1}")
 print(f"part 2: {ans2}")
