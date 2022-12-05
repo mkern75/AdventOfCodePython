28df25d06a8b2e969703c65d68eac55d49704d68
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-12-05 17:30:09 +0000

    2019 Day 16 including part 2

--- year2019/Day16.py
+++ year2019/Day16.py
@@ -1,40 +1,33 @@
-from utils import load_word, tic, toc
-
 INPUT_FILE = "./year2019/data/day16.txt"
+data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
+signal_init = data[0]
+
 PATTERN = [0, 1, 0, -1]
 
 
-def one_phase(l):
-    n = len(l)
-    ll = [0] * n
-    print(len(l))
-    for i in range(n):
-        x = 0
-        if i % 10000 == 0:
-            print(i, len(l))
-        for j in range(i, n):
-            idx = ((j + 1) % ((i + 1) * 4)) // (i + 1)
-            if idx == 1:
-                x += l[j]
-            elif idx == 3:
-                x -= l[j]
-        ll[i] = abs(x) % 10
-    return ll
+def flawed_frequency_transmission(signal, from_index=0):
+    n = len(signal)
+    signal_new = [0] * (n - 1) + [signal[n - 1]]
+    # bottom half is fast
+    for i in range(n - 2, max(n // 2, from_index) - 1, -1):
+        signal_new[i] = (signal_new[i + 1] + signal[i]) % 10
+    # top half is much slower
+    for i in range(from_index, n // 2):
+        signal_new[i] = abs(sum(PATTERN[((j + 1) % ((i + 1) * 4)) // (i + 1)] * signal[j] for j in range(n))) % 10
+    return signal_new
 
-# tic()
-w = load_word(INPUT_FILE)
-#
-# l = [int(i) for i in w]
-#
-# for p in range(1, 101):
-#     l = one_phase(l)
-# ans1 = "".join([str(i) for i in l[0:8]])
-# print(f"part 1: {ans1}   ({toc():.3f}s)")
 
-tic()
-l = []
-for _ in range(10000):
-    l.extend([int(i) for i in w])
-for p in range(1, 101):
-    l = one_phase(l)
-    print(f"phase: {p}   ({toc():.3f}s)")
+# part 1
+signal = [int(i) for i in signal_init]
+for _ in range(100):
+    signal = flawed_frequency_transmission(signal)
+ans1 = "".join(str(i) for i in signal[:8])
+print(f"part 1: {ans1}")
+
+# part 2
+offset = int(signal_init[:7])
+signal = [int(i) for _ in range(10_000) for i in signal_init]
+for _ in range(100):
+    signal = flawed_frequency_transmission(signal, offset)
+ans2 = "".join(str(i) for i in signal[offset:offset + 8])
+print(f"part 2: {ans2}")
