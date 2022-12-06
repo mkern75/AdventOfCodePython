5228fe3380c8907f71cf976ecf651d86da83521a
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-12-06 21:13:35 +0000

    2019 Day 22 finally complete

--- year2019/Day22.py
+++ year2019/Day22.py
@@ -1,35 +1,41 @@
-from utils import load_lines
 import re
 
 INPUT_FILE = "./year2019/data/day22.txt"
-N_CARDS = 10_007
+shuffle_process = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
 
 
-def deal_into_new_stack(stack):
-    return stack[::-1]
+def get_shuffle_coefficients(shuffle_process, n_cards, repeats=1):
+    """Represent (overall) shuffle as 'y=a*x+b MOD n_cards' where x is the initial and y the final position"""
+    a, b = 1, 0
+    for shuffle in shuffle_process:
+        if shuffle == "deal into new stack":
+            a = (-a) % n_cards
+            b = ((n_cards - 1) - b) % n_cards
+        elif x := re.search(r"deal with increment (.*)", shuffle):
+            inc = int(x.groups()[0])
+            a = (a * inc) % n_cards
+            b = (b * inc) % n_cards
+        elif x := re.search(r"cut (.*)", shuffle):
+            cut = int(x.groups()[0])
+            a = a % n_cards
+            b = (b - cut) % n_cards
+    if repeats > 1:  # adjust for repetitions of shuffle process
+        if a == 1:
+            a, b = a, (b * repeats) % n_cards
+        else:
+            a, b = pow(a, repeats, n_cards), (b * (pow(a, repeats, n_cards) - 1) * pow(a - 1, -1, n_cards)) % n_cards
+    return a, b
 
 
-def cut_cards(stack, n):
-    return stack[n:] + stack[:n]
+# part 1
+N = 10_007
+a, b = get_shuffle_coefficients(shuffle_process, N)
+ans1 = (a * 2019 + b) % N
+print(f"part 1: {ans1}")
 
-
-def deal_with_increment(stack, n):
-    stack_new, pos = [0] * len(stack), 0
-    for card in stack:
-        stack_new[pos] = card
-        pos = (pos + n) % len(stack)
-    return stack_new
-
-
-stack = [i for i in range(N_CARDS)]
-for line in load_lines(INPUT_FILE):
-    if line == "deal into new stack":
-        stack = deal_into_new_stack(stack)
-    elif x := re.search(r"deal with increment (.*)", line):
-        n = int(x.groups()[0])
-        stack = deal_with_increment(stack, n)
-    elif x := re.search(r"cut (.*)", line):
-        n = int(x.groups()[0])
-        stack = cut_cards(stack, n)
-
-print("part 1:", stack.index(2019))
+# part 2
+N = 119_315_717_514_047
+repeats = 101_741_582_076_661
+a, b = get_shuffle_coefficients(shuffle_process, N, repeats)
+ans2 = ((2020 - b) * pow(a, -1, N)) % N  # ax+b = 2020 MOD N  =>  x = (2020-b) / a  MOD N
+print(f"part 2: {ans2}")
