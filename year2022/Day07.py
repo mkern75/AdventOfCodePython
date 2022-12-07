98a8f2b4031af7cf2b610deabfe63931fbd5dbab
mkern75 <82466426+mkern75@users.noreply.github.com>
2022-12-07 17:36:12 +0000

    shorter, improved version for day 7

--- year2022/Day07.py
+++ year2022/Day07.py
@@ -1,67 +1,31 @@
-from collections import namedtuple
-
-directory = namedtuple("dir", "name parent_dir child_dirs files")
-file = namedtuple("file", "name size")
-
 INPUT_FILE = "./year2022/data/day07.txt"
-terminal_output = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
+data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
+
+root = curr = ("/",)  # folder = tuple of levels from root down to the folder name itself
+fs = {root: [[], [], -1]}  # fs = map: folder -> [sub-folders, files, total_size]
+for line in data:
+    if line == "$ cd /":
+        curr = root
+    elif line == "$ cd .." and len(curr) > 1:
+        curr = curr[:-1]
+    elif line.startswith("$ cd"):
+        curr += (line[5:],)
+    elif line.startswith("$"):
+        pass
+    elif line.startswith("dir"):
+        folder = curr + (line[4:],)
+        fs[folder] = [[], [], -1]
+        fs[curr][0] += [folder]
+    else:
+        sz, file = line.split()
+        fs[curr][1] += [(file, int(sz))]
 
 
-def parse(terminal_output):
-    root = directory("/", None, {}, {})
-    current_dir = root
-    for line in terminal_output:
-        if line.startswith("$ "):
-            command = line[2:]
-            if command == "cd /":
-                current_dir = root
-            elif command == "cd ..":
-                if current_dir.parent_dir is not None:
-                    current_dir = current_dir.parent_dir
-            elif command.startswith("cd "):
-                child_dir_name = command[3:]
-                if child_dir_name not in current_dir.child_dirs:
-                    child_dir = directory(child_dir_name, current_dir, {}, {})
-                    current_dir.child_dirs[child_dir_name] = child_dir
-                current_dir = current_dir.child_dirs[child_dir_name]
-            else:
-                pass
-        else:
-            if line.startswith("dir "):
-                child_dir_name = line[4:]
-                if child_dir_name not in current_dir.child_dirs:
-                    child_dir = directory(child_dir_name, current_dir, {}, {})
-                    current_dir.child_dirs[child_dir_name] = child_dir
-            else:
-                size, file = line.split()
-                if file not in current_dir.files:
-                    current_dir.files[file] = int(size)
-    return root
+def size(d):
+    if fs[d][2] == -1:
+        fs[d][2] = sum(s for (_, s) in fs[d][1]) + sum(size(c) for c in fs[d][0])
+    return fs[d][2]
 
 
-# to be improved to avoid recomputation
-def total_size(d):
-    return sum(s for s in d.files.values()) + sum(total_size(c) for c in d.child_dirs.values())
-
-
-def part_1(d):
-    return (total_size(d) if total_size(d) <= 100_000 else 0) + sum(part_1(c) for c in d.child_dirs.values())
-
-
-def part_2(d):
-    to_free = 30_000_000 - 70_000_000 + total_size(d)
-    if to_free <= 0:
-        return None
-    smallest = total_size(d)
-    queue = [d]
-    while queue:
-        x = queue.pop()
-        if to_free <= total_size(x) < smallest:
-            smallest = total_size(x)
-        queue.extend(x.child_dirs.values())
-    return smallest
-
-
-root = parse(terminal_output)
-print(f"part 1: {part_1(root)}")
-print(f"part 2: {part_2(root)}")
+print(f"part 1: {sum(size(d) for d in fs if size(d) <= 100_000)}")
+print(f"part 2: {min(size(d) for d in fs if size(root) - size(d) + 30_000_000 <= 70_000_000)}")
