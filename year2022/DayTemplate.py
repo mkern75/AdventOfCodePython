2036379e1e2ae90e50640a66021398f51f5050f0
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-12-08 18:40:26 +0000

    Update DayTemplate.py

--- year2022/DayTemplate.py
+++ year2022/DayTemplate.py
@@ -1,13 +1,24 @@
 import utils
 
-INPUT_FILE = "./year2022/data/day04test.txt"
-# INPUT_FILE = "./year2022/data/day04.txt"
-data = utils.load_lines(INPUT_FILE)
+INPUT_FILE = "./year2022/data/day09test.txt"
+# INPUT_FILE = "./year2022/data/day09.txt"
+
+lines = utils.load_lines(INPUT_FILE)
+# words = utils.load_words(INPUT_FILE)
+# nums = utils.load_numbers(INPUT_FILE)
+# grid = utils.load_grid(INPUT_FILE)
+# grid = utils.load_int_grid(INPUT_FILE)
+# R, C = len(grid), len(grid[0])
+# blocks = utils.load_text_blocks()
 
 ans1, ans2 = 0, 0
 
-for line in data:
+for line in lines:
     pass
+
+# for r in range(R):
+#     for c in range(C):
+#         pass
 
 print(f"part 1: {ans1}")
 print(f"part 2: {ans2}")
