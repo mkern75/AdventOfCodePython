a4915e9be886a0e1f1f27c05cb85aa07ff4feba8
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-12-08 06:54:24 +0000

    use of any() for part 1

--- year2022/Day08.py
+++ year2022/Day08.py
@@ -5,13 +5,11 @@ R, C = len(grid), len(grid[0])
 ans1, ans2 = 0, 0
 for r in range(R):
     for c in range(C):
-        # part 1
-        visible = sum(1 for cc in range(0, c) if grid[r][cc] >= grid[r][c]) == 0
-        visible |= sum(1 for cc in range(c + 1, C) if grid[r][cc] >= grid[r][c]) == 0
-        visible |= sum(1 for rr in range(0, r) if grid[rr][c] >= grid[r][c]) == 0
-        visible |= sum(1 for rr in range(r + 1, R) if grid[rr][c] >= grid[r][c]) == 0
+        visible = not any(grid[r][cc] >= grid[r][c] for cc in range(0, c))
+        visible |= not any(grid[r][cc] >= grid[r][c] for cc in range(c + 1, C))
+        visible |= not any(grid[rr][c] >= grid[r][c] for rr in range(0, r))
+        visible |= not any(grid[rr][c] >= grid[r][c] for rr in range(r + 1, R))
         ans1 += int(visible)
-        # part 2
         score = abs(c - next((cc for cc in range(c - 1, -1, -1) if grid[r][cc] >= grid[r][c]), 0))
         score *= abs(c - next((cc for cc in range(c + 1, C) if grid[r][cc] >= grid[r][c]), C - 1))
         score *= abs(r - next((rr for rr in range(r - 1, -1, -1) if grid[rr][c] >= grid[r][c]), 0))
