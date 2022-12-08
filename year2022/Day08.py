fda3fe68b2383a13681053fd6baba28a404a5045
mkern75 <82466426+mkern75@users.noreply.github.com>
2022-12-08 05:52:12 +0000

    variable renaming

--- year2022/Day08.py
+++ year2022/Day08.py
@@ -12,10 +12,10 @@ for r in range(R):
         visible |= sum(1 for rr in range(r + 1, R) if grid[rr][c] >= grid[r][c]) == 0
         ans1 += int(visible)
         # part 2
-        s = abs(c - next((cc for cc in range(c - 1, -1, -1) if grid[r][cc] >= grid[r][c]), 0))
-        s *= abs(c - next((cc for cc in range(c + 1, C) if grid[r][cc] >= grid[r][c]), C - 1))
-        s *= abs(r - next((rr for rr in range(r - 1, -1, -1) if grid[rr][c] >= grid[r][c]), 0))
-        s *= abs(r - next((rr for rr in range(r + 1, R) if grid[rr][c] >= grid[r][c]), R - 1))
-        ans2 = max(ans2, s)
+        score = abs(c - next((cc for cc in range(c - 1, -1, -1) if grid[r][cc] >= grid[r][c]), 0))
+        score *= abs(c - next((cc for cc in range(c + 1, C) if grid[r][cc] >= grid[r][c]), C - 1))
+        score *= abs(r - next((rr for rr in range(r - 1, -1, -1) if grid[rr][c] >= grid[r][c]), 0))
+        score *= abs(r - next((rr for rr in range(r + 1, R) if grid[rr][c] >= grid[r][c]), R - 1))
+        ans2 = max(ans2, score)
 print(f"part 1: {ans1}")
 print(f"part 2: {ans2}")
