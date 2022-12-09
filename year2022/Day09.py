aaa1389766d635c36c8581d50165c060674bed24
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-12-09 06:19:13 +0000

    knot positions as tuples

--- year2022/Day09.py
+++ year2022/Day09.py
@@ -11,25 +11,25 @@ def sgn(n):
 def follow(head, tail):
     dx, dy = head[0] - tail[0], head[1] - tail[1]
     if abs(dx) == 2 and dy == 0:
-        tail[0] += sgn(dx)
+        return tail[0] + sgn(dx), tail[1]
     elif abs(dx) == 0 and dy == 2:
-        tail[1] += sgn(dy)
+        return tail[0], tail[1] + sgn(dy)
     elif abs(dx) >= 2 or abs(dy) >= 2:
-        tail[0] += sgn(dx)
-        tail[1] += sgn(dy)
+        return tail[0] + sgn(dx), tail[1] + sgn(dy)
+    else:
+        return tail
 
 
 def n_positions(n_knots):
-    knots = [[0, 0] for _ in range(n_knots)]
-    visited = {tuple(knots[-1])}
+    knots = [(0, 0) for _ in range(n_knots)]
+    visited = {knots[-1]}
     for motion in data:
         direction, steps = motion.split()
         for _ in range(int(steps)):
-            knots[0][0] += MOVE[direction][0]
-            knots[0][1] += MOVE[direction][1]
+            knots[0] = (knots[0][0] + MOVE[direction][0], knots[0][1] + MOVE[direction][1])
             for i in range(1, n_knots):
-                follow(knots[i - 1], knots[i])
-            visited |= {tuple(knots[-1])}
+                knots[i] = follow(knots[i - 1], knots[i])
+            visited |= {knots[-1]}
     return len(visited)
 
 
