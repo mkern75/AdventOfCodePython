03fa401a178d2576cac697dfd2c5e90976c7f480
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-12-10 17:50:10 +0000

    better readability

--- year2022/Day10.py
+++ year2022/Day10.py
@@ -1,15 +1,19 @@
 INPUT_FILE = "./year2022/data/day10.txt"
 data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
 
-X = [None, 1]
+x = [None, 1]
 for instr in data:
-    X += [X[-1]]
-    if instr.startswith("addx"):
-        X += [X[-1] + int(instr[5:])]
-print(f"part 1: {sum(c * X[c] for c in range(20, len(X), 40))}")
+    if instr == "noop":
+        x += [x[-1]]
+    elif instr.startswith("addx"):
+        x += [x[-1]]
+        x += [x[-1] + int(instr[5:])]
+    else:
+        assert False
+print(f"part 1: {sum(c * x[c] for c in range(20, len(x), 40))}")
 
 print(f"part 2:")
 for i in range(0, 240):
-    print("#" if X[i + 1] - 1 <= i % 40 <= X[i + 1] + 1 else ".", end="")
+    print("##" if abs(i % 40 - x[i + 1]) <= 1 else "  ", end="")
     if i % 40 == 39:
         print()
