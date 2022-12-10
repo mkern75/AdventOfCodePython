5168957044114928572b9c0d7332a04246a55771
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-12-10 17:56:08 +0000

    better readability v2

--- year2022/Day10.py
+++ year2022/Day10.py
@@ -1,3 +1,5 @@
+from colorama import Back, Style
+
 INPUT_FILE = "./year2022/data/day10.txt"
 data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
 
@@ -14,6 +16,6 @@ print(f"part 1: {sum(c * x[c] for c in range(20, len(x), 40))}")
 
 print(f"part 2:")
 for i in range(0, 240):
-    print("##" if abs(i % 40 - x[i + 1]) <= 1 else "  ", end="")
+    print(Back.WHITE + "##" + Style.RESET_ALL if abs(i % 40 - x[i + 1]) <= 1 else "..", end="")
     if i % 40 == 39:
         print()
