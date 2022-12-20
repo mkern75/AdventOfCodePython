34abc08d3d1fa26f8e48634485b53590c70d6484
mkern75 <82466426+mkern75@users.noreply.github.com>
2022-12-20 17:18:48 +0000

    cleaning up, variable renaming

--- year2022/Day20.py
+++ year2022/Day20.py
@@ -4,22 +4,20 @@ N = len(data)
 
 
 def single_move(nums, num):
-    pos = nums.index(num)
-    pos_new = (pos + num[0]) % (N - 1)
-    if pos_new >= pos:
-        return nums[:pos] + nums[pos + 1:pos_new + 1] + [num] + nums[pos_new + 1:]
-    else:
-        return nums[:pos_new] + [num] + nums[pos_new:pos] + nums[pos + 1:]
+    idx = nums.index(num)
+    idx_new = (idx + num[0]) % (N - 1)
+    nums.remove(num)
+    nums.insert(idx_new, num)
 
 
 # part 1
 orig = [(int(l), i) for i, l in enumerate(data)]
 nums = [num for num in orig]
 for num in orig:
-    nums = single_move(nums, num)
+    single_move(nums, num)
 zero = next(num for num in orig if num[0] == 0)
-pzero = nums.index(zero)
-ans1 = nums[(pzero + 1000) % N][0] + nums[(pzero + 2000) % N][0] + nums[(pzero + 3000) % N][0]
+idx_zero = nums.index(zero)
+ans1 = nums[(idx_zero + 1000) % N][0] + nums[(idx_zero + 2000) % N][0] + nums[(idx_zero + 3000) % N][0]
 print(f"part 1: {ans1}")
 
 # part 2
@@ -27,8 +25,8 @@ orig = [(int(l) * 811589153, i) for i, l in enumerate(data)]
 nums = [num for num in orig]
 for _ in range(10):
     for num in orig:
-        nums = single_move(nums, num)
+        single_move(nums, num)
 zero = next(num for num in orig if num[0] == 0)
-pzero = nums.index(zero)
-ans2 = nums[(pzero + 1000) % N][0] + nums[(pzero + 2000) % N][0] + nums[(pzero + 3000) % N][0]
+idx_zero = nums.index(zero)
+ans2 = nums[(idx_zero + 1000) % N][0] + nums[(idx_zero + 2000) % N][0] + nums[(idx_zero + 3000) % N][0]
 print(f"part 2: {ans2}")
