ba46f24eae42d0e3d8aebae1e18425f3fd2424a6
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2022-12-20 21:23:43 +0000

    one more change

--- year2022/Day20.py
+++ year2022/Day20.py
@@ -1,19 +1,18 @@
 INPUT_FILE = "./year2022/data/day20.txt"
 data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
-N = len(data)
 
 
 def mix(nums, order):
     for n in order:
         idx = nums.index(n)
-        idx_new = (idx + n[0]) % (N - 1)
+        idx_new = (idx + n[0]) % (len(order) - 1)
         nums.remove(n)
         nums.insert(idx_new, n)
 
 
 def sum_of_grove_coordinates(nums):
     idx_zero = next(i for i, n in enumerate(nums) if n[0] == 0)
-    return sum(nums[(idx_zero + 1000 * i) % N][0] for i in range(1, 4))
+    return sum(nums[(idx_zero + 1000 * i) % len(nums)][0] for i in range(1, 4))
 
 
 # part 1
