5c942a005dc705f40c1475c2f0c8045f01ad580d
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-02 05:45:30 +0000

    2023 day 02

--- year2023/Day02.py
+++ year2023/Day02.py
@@ -5,19 +5,14 @@ data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
 
 ans1, ans2 = 0, 0
 
-for line in data:
-    header, info = line.split(":")
-    game_id = int(header.split()[1])
-    picks = re.split(r"[;,]", info)
-    possible = True
+for game_id, line in enumerate(data, 1):
     max_cnt = {"g": 0, "r": 0, "b": 0}
+    picks = re.split(r"[;,]", line.split(":")[1])
     for pick in picks:
         cnt, col = pick.split()
         cnt, col = int(cnt), col[0]
-        if not (col == "r" and cnt <= 12 or col == "g" and cnt <= 13 or col == "b" and cnt <= 14):
-            possible = False
         max_cnt[col] = max(max_cnt[col], cnt)
-    if possible:
+    if max_cnt["r"] <= 12 and max_cnt["g"] <= 13 and max_cnt["b"] <= 14:
         ans1 += game_id
     ans2 += max_cnt["r"] * max_cnt["g"] * max_cnt["b"]
 
