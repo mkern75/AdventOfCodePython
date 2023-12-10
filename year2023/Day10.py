6d9edf967aee3dfd9bbfb4e9046f3ec401b30c84
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-10 11:02:45 +0000

    2023 day 10 further improvement

--- year2023/Day10.py
+++ year2023/Day10.py
@@ -1,63 +1,49 @@
-from collections import defaultdict
-
 INPUT_FILE = "./year2023/data/day10.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
 
-CONNECTS = {"-": [(0, -1), (0, +1)], "|": [(-1, 0), (+1, 0)], "F": [(+1, 0), (0, +1)], "7": [(+1, 0), (0, -1)],
-            "L": [(-1, 0), (0, +1)], "J": [(-1, 0), (0, -1)]}
-PIPES = CONNECTS.keys()
+PIPE_CONNECTIONS = {"-": [(0, -1), (0, +1)], "|": [(-1, 0), (+1, 0)], "F": [(+1, 0), (0, +1)],
+                    "7": [(+1, 0), (0, -1)], "L": [(-1, 0), (0, +1)], "J": [(-1, 0), (0, -1)]}
 
-# grid
-grid = defaultdict(lambda: ".")
-R, C = len(data), len(data[0])
-for r, line in enumerate(data):
-    for c, x in enumerate(line):
-        grid[r, c] = x
-
-# start row and column
-r_start, c_start = next((r, c) for r in range(R) for c in range(C) if grid[r, c] == "S")
+# grid and start row/column
+grid = [list(line) for line in data]
+R, C = len(grid), len(grid[0])
+r_start, c_start = next((r, c) for r in range(R) for c in range(C) if grid[r][c] == "S")
 
 # find loop
-for start_pipe in PIPES:
-    loop = [(r_start, c_start), (r_start + CONNECTS[start_pipe][0][0], c_start + CONNECTS[start_pipe][0][1])]
+for start_pipe, start_connect in PIPE_CONNECTIONS.items():
+    path = [(r_start, c_start), (r_start + start_connect[0][0], c_start + start_connect[0][1])]
     loop_found = False
     while True:
-        if loop[-1] == loop[0]:
+        r, c = path[-1]
+        if not (0 <= r < R and 0 <= c < C) or grid[r][c] == ".":
+            break
+        if (r, c) == (r_start, c_start):
             loop_found = True
             break
-        if grid[loop[-1]] not in PIPES:
-            break
-        (r, c), pipe = loop[-1], grid[loop[-1]]
-        if loop[-2] == (r + CONNECTS[pipe][0][0], c + CONNECTS[pipe][0][1]):
-            loop += [(r + CONNECTS[pipe][1][0], c + CONNECTS[pipe][1][1])]
-        elif loop[-2] == (r + CONNECTS[pipe][1][0], c + CONNECTS[pipe][1][1]):
-            loop += [(r + CONNECTS[pipe][0][0], c + CONNECTS[pipe][0][1])]
+        connect = PIPE_CONNECTIONS[grid[r][c]]
+        if path[-2] == (r + connect[0][0], c + connect[0][1]):
+            path += [(r + connect[1][0], c + connect[1][1])]
+        elif path[-2] == (r + connect[1][0], c + connect[1][1]):
+            path += [(r + connect[0][0], c + connect[0][1])]
         else:
             break
     if loop_found:
-        grid[r_start, c_start] = start_pipe
-        loop = set(loop)
+        grid[r_start][c_start] = start_pipe
+        loop = set(path)
+        ans1 = len(loop) // 2
         break
 else:
     assert False
 
-ans1 = len(loop) // 2
-
+# adapted version of ray-casting algorithm for point-in-polygon problem
 ans2 = 0
 for r in range(R):
-    # count how often the loop crosses the current row
-    # the row is crossed when encountering loop segments "|", "L-7" or "F-J" (any number of "-")
-    # if the current row has been crossed an odd number of times when reaching a column, that tile is inside the loop
-    is_inside, prev_pipe = 0, "."
+    is_inside, prev = 0, "."
     for c in range(C):
-        if (r, c) in loop:
-            if grid[r, c] == "|":
-                is_inside = 1 - is_inside
-            elif grid[r, c] in ["L", "F"]:
-                prev_pipe = grid[r, c]
-            elif prev_pipe == "L" and grid[r, c] == "7":
-                is_inside = 1 - is_inside
-            elif prev_pipe == "F" and grid[r, c] == "J":
+        if (r, c) in path:
+            if grid[r][c] in ["L", "F"]:
+                prev = grid[r][c]
+            elif grid[r][c] == "|" or (prev == "L" and grid[r][c] == "7") or (prev == "F" and grid[r][c] == "J"):
                 is_inside = 1 - is_inside
         else:
             ans2 += is_inside
