c5ea946499d63f78c3afe9bb3a54b332f5e95cf4
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-11 06:20:07 +0000

    2023 11 final small change

--- year2023/Day11.py
+++ year2023/Day11.py
@@ -1,21 +1,20 @@
 INPUT_FILE = "./year2023/data/day11.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
 
-n_rows = len(data)
-n_cols = len(data[0])
+R, C = len(data), len(data[0])
 
-galaxies = [(r, c) for r in range(n_rows) for c in range(n_cols) if data[r][c] == "#"]
-n_galaxies = len(galaxies)
+galaxies = [(r, c) for r in range(R) for c in range(C) if data[r][c] == "#"]
+N = len(galaxies)
 
-empty_rows = set(range(n_rows)) - set(r for r, _ in galaxies)
-empty_cols = set(range(n_cols)) - set(c for _, c in galaxies)
+empty_rows = set(range(R)) - set(r for r, _ in galaxies)
+empty_cols = set(range(C)) - set(c for _, c in galaxies)
 
 
 def calc_sum_shortest_dist(expansion_factor):
     res = 0
-    for i in range(n_galaxies - 1):
+    for i in range(N - 1):
         r1, c1 = galaxies[i]
-        for j in range(i + 1, n_galaxies):
+        for j in range(i + 1, N):
             r2, c2 = galaxies[j]
             res += abs(r1 - r2) + abs(c1 - c2)
             res += (expansion_factor - 1) * sum(1 for r in empty_rows if min(r1, r2) < r < max(r1, r2))
