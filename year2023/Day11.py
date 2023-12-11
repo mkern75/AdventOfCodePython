e073d1810298ae6ca55aaccfb0dec6fcc922611e
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-11 05:46:29 +0000

    2023 day 11 improvement

--- year2023/Day11.py
+++ year2023/Day11.py
@@ -1,14 +1,17 @@
 INPUT_FILE = "./year2023/data/day11.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
 
+n_rows = len(data)
+n_cols = len(data[0])
 
-def solve(image, expansion_factor=1):
-    n_rows = len(image)
-    n_cols = len(image[0])
-    empty_rows = [r for r in range(n_rows) if all(data[r][c] == "." for c in range(n_cols))]
-    empty_cols = [c for c in range(n_cols) if all(data[r][c] == "." for r in range(n_rows))]
-    galaxies = [(r, c) for r in range(n_rows) for c in range(n_cols) if data[r][c] == "#"]
-    n_galaxies = len(galaxies)
+galaxies = [(r, c) for r in range(n_rows) for c in range(n_cols) if data[r][c] == "#"]
+n_galaxies = len(galaxies)
+
+empty_rows = set(range(n_rows)) - set(r for r, _ in galaxies)
+empty_cols = set(range(n_cols)) - set(c for _, c in galaxies)
+
+
+def calc_sum_shortest_dist(expansion_factor):
     res = 0
     for i in range(n_galaxies - 1):
         r1, c1 = galaxies[i]
@@ -20,5 +23,5 @@ def solve(image, expansion_factor=1):
     return res
 
 
-print(f"part 1: {solve(data, 2)}")
-print(f"part 2: {solve(data, 1_000_000)}")
+print(f"part 1: {calc_sum_shortest_dist(2)}")
+print(f"part 2: {calc_sum_shortest_dist(1_000_000)}")
