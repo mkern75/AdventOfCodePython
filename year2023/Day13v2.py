dc730b80d2d7c1010b090e28d718542be7d43300
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-13 11:24:52 +0000

    2023 day 13 v2 improvement

--- year2023/Day13v2.py
+++ year2023/Day13v2.py
@@ -9,7 +9,7 @@ for line in data:
         grids += [[]]
 
 
-def delta_row_symmetrie(grid, row):
+def delta_row_symmetry(grid, row):
     delta = 0
     for r1 in range(row + 1):
         r2 = row + 1 + (row - r1)
@@ -18,30 +18,21 @@ def delta_row_symmetrie(grid, row):
     return delta
 
 
-def delta_col_symmetrie(grid, col):
-    delta = 0
-    for c1 in range(col + 1):
-        c2 = col + 1 + (col - c1)
-        if c2 < len(grid[0]):
-            delta += sum(1 for r in range(len(grid)) if grid[r][c1] != grid[r][c2])
-    return delta
+def delta_col_symmetry(grid, col):
+    return delta_row_symmetry([list(x) for x in zip(*grid)], col)
 
 
 ans1, ans2 = 0, 0
 
 for grid in grids:
     for row in range(len(grid) - 1):
-        delta = delta_row_symmetrie(grid, row)
-        if delta == 0:
-            ans1 += 100 * (row + 1)
-        elif delta == 1:
-            ans2 += 100 * (row + 1)
+        delta = delta_row_symmetry(grid, row)
+        ans1 += 100 * (row + 1) if delta == 0 else 0
+        ans2 += 100 * (row + 1) if delta == 1 else 0
     for col in range(len(grid[0]) - 1):
-        delta = delta_col_symmetrie(grid, col)
-        if delta == 0:
-            ans1 += col + 1
-        elif delta == 1:
-            ans2 += col + 1
+        delta = delta_col_symmetry(grid, col)
+        ans1 += col + 1 if delta == 0 else 0
+        ans2 += col + 1 if delta == 1 else 0
 
 print(f"part 1: {ans1}")
 print(f"part 2: {ans2}")
