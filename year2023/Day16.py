932058460c718a9259372c806f04b999c9021b73
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-16 06:22:09 +0000

    2023 day 16 improvement

--- year2023/Day16.py
+++ year2023/Day16.py
@@ -5,27 +5,24 @@ grid = [list(line) for line in data]
 R, C = len(grid), len(grid[0])
 
 
-def simulate(r, c, dr, dc):
-    energised = [[0] * C for _ in range(R)]
+def simulate(r_init, c_init, dr_init, dc_init):
     seen = set()
-    q = [(r, c, dr, dc)]
+    q = [(r_init, c_init, dr_init, dc_init)]
     while q:
         r, c, dr, dc = q.pop()
-        if not (0 <= r < R and 0 <= c < C) or (r, c, dr, dc) in seen:
-            continue
-        energised[r][c] = 1
-        seen |= {(r, c, dr, dc)}
-        if grid[r][c] == "\\":
-            q += [(r + dc, c + dr, dc, dr)]
-        elif grid[r][c] == "/":
-            q += [(r - dc, c - dr, -dc, -dr)]
-        elif grid[r][c] == "-" and dr != 0:
-            q += [(r, c - 1, 0, -1), (r, c + 1, 0, 1)]
-        elif grid[r][c] == "|" and dc != 0:
-            q += [(r + 1, c, 1, 0), (r - 1, c, -1, 0)]
-        else:
-            q += [(r + dr, c + dc, dr, dc)]  # default
-    return sum(sum(row) for row in energised)
+        if 0 <= r < R and 0 <= c < C and (r, c, dr, dc) not in seen:
+            seen |= {(r, c, dr, dc)}
+            if grid[r][c] == "\\":
+                q += [(r + dc, c + dr, dc, dr)]
+            elif grid[r][c] == "/":
+                q += [(r - dc, c - dr, -dc, -dr)]
+            elif grid[r][c] == "-" and dr != 0:
+                q += [(r, c - 1, 0, -1), (r, c + 1, 0, 1)]
+            elif grid[r][c] == "|" and dc != 0:
+                q += [(r + 1, c, 1, 0), (r - 1, c, -1, 0)]
+            else:
+                q += [(r + dr, c + dc, dr, dc)]  # default
+    return len({(r, c) for r, c, _, _ in seen})
 
 
 ans1 = simulate(0, 0, 0, 1)
