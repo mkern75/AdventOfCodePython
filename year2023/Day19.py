ad62d6bba497fc81a4b05fd0a65d99f20069e144
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-19 18:00:03 +0000

    2023 day 19 refinement

--- year2023/Day19.py
+++ year2023/Day19.py
@@ -2,52 +2,38 @@ from math import prod
 from copy import deepcopy
 
 INPUT_FILE = "./year2023/data/day19.txt"
-data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
+blocks = [block.splitlines() for block in open(INPUT_FILE, "r").read().split("\n\n")]
 
-# split into 2 blocks
-blocks = [[]]
-for line in data:
-    if line:
-        blocks[-1] += [line]
-    else:
-        blocks += [[]]
-
-# read workflows
 workflows = {"A": ["A"], "R": ["R"]}
 for line in blocks[0]:
-    workflow_name, info = line.split("{")
-    workflows[workflow_name] = []
-    for descr in info[:-1].split(","):
-        if ":" in descr:
-            before, target_rule = descr.split(":")
-            workflows[workflow_name] += [(before[0], before[1], int(before[2:]), target_rule)]
+    wid, rules_info = line.split("{")
+    workflows[wid] = []
+    for info in rules_info[:-1].split(","):
+        if ":" in info:
+            before, target_wid = info.split(":")
+            workflows[wid] += [(before[0], before[1], int(before[2:]), target_wid)]
         else:
-            workflows[workflow_name] += [descr]
+            workflows[wid] += [info]
 
 
 def count_accepted_combinations(x_min, x_max, m_min, m_max, a_min, a_max, s_min, s_max):
     res = 0
     q = [("in", {"x": (x_min, x_max), "m": (m_min, m_max), "a": (a_min, a_max), "s": (s_min, s_max)})]
     while q:
-        workflow_name, var_vals = q.pop()
-
-        for rule in workflows[workflow_name]:
+        wid, var_vals = q.pop()
+        for rule in workflows[wid]:
             if any(mx < mn for mn, mx in var_vals.values()):
                 break
-
             elif rule == "A":
                 res += prod(mx - mn + 1 for mn, mx in var_vals.values())
                 break
-
             elif rule == "R":
                 break
-
             elif rule in workflows:
                 q += [(rule, deepcopy(var_vals))]
                 break
-
-            elif len(rule) == 4:
-                var, comp, threshold, workflow_name_next = rule
+            elif type(rule) is tuple:
+                var, comp, threshold, wid_next = rule
                 var_vals_next = deepcopy(var_vals)
                 if comp == ">":
                     var_vals_next[var] = (max(threshold + 1, var_vals[var][0]), var_vals[var][1])
@@ -55,7 +41,7 @@ def count_accepted_combinations(x_min, x_max, m_min, m_max, a_min, a_max, s_min,
                 elif comp == "<":
                     var_vals_next[var] = (var_vals[var][0], min(threshold - 1, var_vals[var][1]))
                     var_vals[var] = (max(var_vals[var][0], threshold), var_vals[var][1])
-                q += [(workflow_name_next, var_vals_next)]
+                q += [(wid_next, var_vals_next)]
     return res
 
 
