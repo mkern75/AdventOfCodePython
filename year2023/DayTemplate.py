a5530fe2a83534f5b6e2ce6f29535b6340723b36
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-19 18:05:27 +0000

    simplification of 2023 template

--- year2023/DayTemplate.py
+++ year2023/DayTemplate.py
@@ -1,28 +1,14 @@
-import utils
-import re
-from collections import defaultdict, deque, Counter
-from functools import lru_cache
-
 # INPUT_FILE = "./year2023/data/day00test.txt"
 INPUT_FILE = "./year2023/data/day00.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
-N = len(data)
-# lines = utils.load_lines(INPUT_FILE)
-# words = utils.load_words(INPUT_FILE)
-# nums = utils.load_numbers(INPUT_FILE)
-# grid = utils.load_grid(INPUT_FILE)
-# grid = utils.load_int_grid(INPUT_FILE)
+# blocks = [block.splitlines() for block in open(INPUT_FILE, "r").read().split("\n\n")]
+# grid = [list(line.rstrip("\n")) for line in open(INPUT_FILE, "r")]
 # R, C = len(grid), len(grid[0])
-# blocks = utils.load_text_blocks()
 
 ans1, ans2 = 0, 0
 
 for line in data:
     pass
-
-# for r in range(R):
-#     for c in range(C):
-#         pass
 
 print(f"part 1: {ans1}")
 print(f"part 2: {ans2}")
