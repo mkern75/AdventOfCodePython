41bc1eb7125a2f8677ba98aa37cbecdef7bfcf54
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-22 08:36:44 +0000

    2023 day 22 further small refinements

--- year2023/Day22.py
+++ year2023/Day22.py
@@ -2,40 +2,47 @@ INPUT_FILE = "./year2023/data/day22.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
 
 GROUND = -1
+X, Y, Z = 0, 1, 2
 
 bricks = []
 for line in data:
-    start, end = line.split("~")
-    bricks += [(list(map(int, start.split(","))), list(map(int, end.split(","))))]
+    coord1, coord2 = line.split("~")
+    bricks += [(list(map(int, coord1.split(","))), list(map(int, coord2.split(","))))]
 N = len(bricks)
 
 
-def do_overlap(b1, b2):
-    return not (b1[1][0] < b2[0][0] or b1[0][0] > b2[1][0] or b1[1][1] < b2[0][1] or b1[0][1] > b2[1][1])
+def is_overlap(brick1, brick2):
+    return not (brick1[1][X] < brick2[0][X] or brick1[0][X] > brick2[1][X] or
+                brick1[1][Y] < brick2[0][Y] or brick1[0][Y] > brick2[1][Y])
 
 
-def top(b):
-    return max(b[0][2], b[1][2])
+def top(brick):
+    return max(brick[0][Z], brick[1][Z])
 
 
-def bottom(b):
-    return min(b[0][2], b[1][2])
+def bottom(brick):
+    return min(brick[0][Z], brick[1][Z])
 
 
-bricks.sort(key=lambda b: bottom(b))
+def move_down(brick, target_bottom_z):
+    fall = bottom(brick) - target_bottom_z
+    brick[0][Z] -= fall
+    brick[1][Z] -= fall
+
+
+# sort bricks by increasing bottom / Z value
+bricks.sort(key=lambda brick: bottom(brick))
+
 rests_on = [[] for _ in range(N)]
-
-for i in range(N):
-    overlaps_below = {j for j in range(i) if do_overlap(bricks[i], bricks[j])}
+for i, brick in enumerate(bricks):
+    overlaps_below = {j for j in range(i) if is_overlap(brick, bricks[j])}
     if overlaps_below:
-        top_max = max(top(bricks[k]) for k in overlaps_below)
-        rests_on[i] += [k for k in overlaps_below if top(bricks[k]) == top_max]
-        fall = bottom(bricks[i]) - (top_max + 1)
+        max_top = max(top(bricks[j]) for j in overlaps_below)
+        rests_on[i] = [j for j in overlaps_below if top(bricks[j]) == max_top]
+        move_down(brick, max_top + 1)
     else:
         rests_on[i] += [GROUND]
-        fall = bottom(bricks[i]) - 1
-    bricks[i][0][2] -= fall
-    bricks[i][1][2] -= fall
+        move_down(brick, 1)
 
 can_be_disintegrated = [True] * N
 for i in range(N):
