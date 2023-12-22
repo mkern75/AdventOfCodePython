a9f006aef6ece2d1640a06158ee2536aa25436ad
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-22 11:28:12 +0000

    2023 day 22 final small improvements

--- year2023/Day22.py
+++ year2023/Day22.py
@@ -1,7 +1,6 @@
 INPUT_FILE = "./year2023/data/day22.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
 
-GROUND = -1
 X, Y, Z = 0, 1, 2
 
 bricks = []
@@ -16,37 +15,27 @@ def is_overlap(brick1, brick2):
                 brick1[1][Y] < brick2[0][Y] or brick1[0][Y] > brick2[1][Y])
 
 
-def top(brick):
-    return max(brick[0][Z], brick[1][Z])
-
-
-def bottom(brick):
-    return min(brick[0][Z], brick[1][Z])
-
-
 def move_down(brick, target_bottom_z):
-    fall = bottom(brick) - target_bottom_z
+    fall = brick[0][Z] - target_bottom_z
     brick[0][Z] -= fall
     brick[1][Z] -= fall
 
 
-# sort bricks by increasing bottom / Z value
-bricks.sort(key=lambda brick: bottom(brick))
+bricks.sort(key=lambda brick: brick[0][Z])
 
 rests_on = [[] for _ in range(N)]
 for i, brick in enumerate(bricks):
     overlaps_below = {j for j in range(i) if is_overlap(brick, bricks[j])}
     if overlaps_below:
-        max_top = max(top(bricks[j]) for j in overlaps_below)
-        rests_on[i] = [j for j in overlaps_below if top(bricks[j]) == max_top]
+        max_top = max(bricks[j][1][Z] for j in overlaps_below)
+        rests_on[i] = [j for j in overlaps_below if bricks[j][1][Z] == max_top]
         move_down(brick, max_top + 1)
     else:
-        rests_on[i] += [GROUND]
         move_down(brick, 1)
 
 can_be_disintegrated = [True] * N
 for i in range(N):
-    if len(rests_on[i]) == 1 and rests_on[i][0] != GROUND:
+    if len(rests_on[i]) == 1:
         can_be_disintegrated[rests_on[i][0]] = False
 ans1 = sum(can_be_disintegrated)
 print(f"part 1: {ans1}")
@@ -56,7 +45,7 @@ for i in range(N):
     if not can_be_disintegrated[i]:
         gone = {i}
         for j in range(i + 1, N):
-            if set(rests_on[j]).issubset(gone):
+            if rests_on[j] and set(rests_on[j]).issubset(gone):
                 gone |= {j}
         ans2 += len(gone) - 1
 print(f"part 2: {ans2}")
