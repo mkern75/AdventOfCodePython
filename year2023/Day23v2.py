b8593cdef4885884ea7d5a6564eee8caeda9e991
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-23 10:32:05 +0000

    2023 day 23 v2 for speed (~1s)

---
+++ year2023/Day23v2.py
@@ -0,0 +1,71 @@
+# v2 for speed: runs in ~1s using pypy on MacBook Pro 2021
+
+from collections import defaultdict
+
+INPUT_FILE = "./year2023/data/day23.txt"
+grid = [list(line.rstrip("\n")) for line in open(INPUT_FILE, "r")]
+R, C = len(grid), len(grid[0])
+
+MOVES1 = {".": [(1, 0), (-1, 0), (0, 1), (0, -1)], ">": [(0, 1)], "<": [(0, -1)], "^": [(-1, 0)], "v": [(1, 0)]}
+MOVES2 = {c: MOVES1["."] for c in ".<>^v"}
+
+# start and end coordinates
+r_start, c_start = 0, next(c for c in range(C) if grid[0][c] == ".")
+r_end, c_end = R - 1, next(c for c in range(C) if grid[R - 1][c] == ".")
+
+#  points-of-interest: only consider grid positions / junctions with an actual choice, plus start and end
+poi = [(r_start, c_start), (r_end, c_end)]
+for r in range(R):
+    for c in range(C):
+        if grid[r][c] != "#":
+            if sum(1 for rn, cn in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)] if
+                   0 <= rn < R and 0 <= cn < C and grid[rn][cn] != "#") > 2:
+                poi += [(r, c)]
+N = len(poi)
+idx = {p: i for i, p in enumerate(poi)}
+start = idx[0, next(c for c in range(C) if grid[0][c] == ".")]
+end = idx[R - 1, next(c for c in range(C) if grid[R - 1][c] == ".")]
+
+
+def build_adjacency_list(moves):
+    adj = defaultdict(list)
+    for i in range(N):
+        visited = set()
+        q = [(poi[i][0], poi[i][1], 0)]
+        while q:
+            r, c, dist = q.pop()
+            if (r, c) not in visited:
+                visited |= {(r, c)}
+                if (r, c) != poi[i] and (r, c) in idx:
+                    adj[i] += [(idx[r, c], dist)]
+                else:
+                    for dr, dc in moves[grid[r][c]]:
+                        rn, cn = r + dr, c + dc
+                        if 0 <= rn < R and 0 <= cn < C and grid[rn][cn] != "#":
+                            q += [(rn, cn, dist + 1)]
+    return adj
+
+
+def find_longest_path(adj):
+    res = 0
+    q = [(start, 0, 1 << start)]
+    while q:
+        v, dist_so_far, visited = q.pop()
+        if v == end:
+            res = max(res, dist_so_far)
+        else:
+            for vn, dist_to_vn in adj[v]:
+                if visited & (1 << vn) == 0:
+                    q += [(vn, dist_so_far + dist_to_vn, visited | (1 << vn))]
+    return res
+
+
+# part 1
+adj1 = build_adjacency_list(MOVES1)
+ans1 = find_longest_path(adj1)
+print(f"part 1: {ans1}")
+
+# part 2
+adj2 = build_adjacency_list(MOVES2)
+ans2 = find_longest_path(adj2)
+print(f"part 2: {ans2}")
