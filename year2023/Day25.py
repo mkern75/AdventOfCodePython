ae8bf8bf0e577f91045a7a69714f2885d4ef1f7f
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2023-12-25 06:52:26 +0000

    2023 day 25 thanks to networkx

--- year2023/Day25.py
+++ year2023/Day25.py
@@ -4,7 +4,6 @@ INPUT_FILE = "./year2023/data/day25.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
 
 graph = nx.DiGraph()
-
 for line in data:
     component, connections = line.split(": ")
     for other_component in connections.split():
@@ -12,13 +11,6 @@ for line in data:
         graph.add_edge(other_component, component, capacity=1.0)
 
 nodes = list(graph.nodes)
-n_nodes = len(nodes)
-
-cnt = 1
-node1 = nodes[0]
-for node2 in nodes[1:]:
-    if nx.minimum_cut_value(graph, node1, node2) > 3:
-        cnt += 1
-
+cnt = 1 + sum(1 for node2 in nodes[1:] if nx.minimum_cut_value(graph, nodes[0], node2) > 3)
 ans1 = cnt * (graph.number_of_nodes() - cnt)
 print(f"part 1: {ans1}")
