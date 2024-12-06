0eb5b2aa29a33f04ba05dedaa73e5b5c2a129462
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2024-12-06 06:00:30 +0000

    day 6 speed up

--- year2024/Day06.py
+++ year2024/Day06.py
@@ -10,23 +10,21 @@ grid = defaultdict(lambda: ".", {(r, c): v for r, row in enumerate(data) for c, 
 rs, cs = next((r, c) for r in range(R) for c in range(C) if grid[r, c] == "^")
 
 r, c, dr, dc = rs, cs, -1, 0
-seen = {hash((rs, cs))}
+seen_p1 = {(rs, cs)}
 while True:
     while grid[r + dr, c + dc] == "#":
         dr, dc = dc, -dr
     r, c = r + dr, c + dc
-    if not (0 <= r < R and 0 <= c < C):
+    if r < 0 or R <= r or c < 0 or C <= c:
         break
-    seen.add(hash((r, c)))
+    seen_p1.add((r, c))
 
-ans1 = len(seen)
+ans1 = len(seen_p1)
 print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
 
 
 def simulate_p2(r_obstr, c_obstr):
-    if r_obstr == rs and c_obstr == cs:
-        return 0
-    if grid[r_obstr, c_obstr] == "#":
+    if r_obstr == rs and c_obstr == cs or grid[r_obstr, c_obstr] == "#":
         return 0
     r, c, dr, dc = rs, cs, -1, 0
     seen = {hash((rs, cs, dr, dc))}
@@ -34,7 +32,7 @@ def simulate_p2(r_obstr, c_obstr):
         while grid[r + dr, c + dc] == "#" or (r + dr == r_obstr and c + dc == c_obstr):
             dr, dc = dc, -dr
         r, c = r + dr, c + dc
-        if not (0 <= r < R and 0 <= c < C):
+        if r < 0 or R <= r or c < 0 or C <= c:
             return 0
         h = hash((r, c, dr, dc))
         if h in seen:
@@ -42,9 +40,5 @@ def simulate_p2(r_obstr, c_obstr):
         seen.add(h)
 
 
-ans2 = 0
-for r_obstr in range(R):
-    for c_obstr in range(C):
-        ans2 += simulate_p2(r_obstr, c_obstr)
-
+ans2 = sum(simulate_p2(r_obstr, c_obstr) for r_obstr, c_obstr in seen_p1)
 print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
