f0d9c0631847b7ebef6013ba4cc51b5afd4211be
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2024-12-07 00:30:57 +0000

    day 6 speed-up

--- year2024/Day06.py
+++ year2024/Day06.py
@@ -1,18 +1,17 @@
 from time import time
-from collections import defaultdict
 
 time_start = time()
 INPUT_FILE = "./year2024/data/day06.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
-R, C = len(data), len(data[0])
-grid = defaultdict(lambda: ".", {(r, c): v for r, row in enumerate(data) for c, v in enumerate(row)})
+grid = [list(line.rstrip("\n")) for line in open(INPUT_FILE, "r")]
+R, C = len(grid), len(grid[0])
 
-rs, cs = next((r, c) for r in range(R) for c in range(C) if grid[r, c] == "^")
+r_start, c_start = next((r, c) for r in range(R) for c in range(C) if grid[r][c] == "^")
 
-r, c, dr, dc = rs, cs, -1, 0
-seen_p1 = {(rs, cs)}
+r, c, dr, dc = r_start, c_start, -1, 0
+seen_p1 = {(r_start, c_start)}
 while True:
-    while grid[r + dr, c + dc] == "#":
+    while 0 <= r + dr < R and 0 <= c + dc < C and grid[r + dr][c + dc] == "#":
         dr, dc = dc, -dr
     r, c = r + dr, c + dc
     if r < 0 or R <= r or c < 0 or C <= c:
@@ -23,13 +22,11 @@ ans1 = len(seen_p1)
 print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
 
 
-def simulate_p2(r_obstr, c_obstr):
-    if r_obstr == rs and c_obstr == cs or grid[r_obstr, c_obstr] == "#":
-        return 0
-    r, c, dr, dc = rs, cs, -1, 0
-    seen = {hash((rs, cs, dr, dc))}
+def check_loop_p2():
+    r, c, dr, dc = r_start, c_start, -1, 0
+    seen = {hash((r_start, c_start, dr, dc))}
     while True:
-        while grid[r + dr, c + dc] == "#" or (r + dr == r_obstr and c + dc == c_obstr):
+        while 0 <= r + dr < R and 0 <= c + dc < C and grid[r + dr][c + dc] == "#":
             dr, dc = dc, -dr
         r, c = r + dr, c + dc
         if r < 0 or R <= r or c < 0 or C <= c:
@@ -40,5 +37,11 @@ def simulate_p2(r_obstr, c_obstr):
         seen.add(h)
 
 
-ans2 = sum(simulate_p2(r_obstr, c_obstr) for r_obstr, c_obstr in seen_p1)
+ans2 = 0
+for r_obstr, c_obstr in seen_p1:
+    if r_obstr == r_start and c_obstr == c_start:
+        continue
+    grid[r_obstr][c_obstr] = "#"
+    ans2 += check_loop_p2()
+    grid[r_obstr][c_obstr] = "."
 print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
