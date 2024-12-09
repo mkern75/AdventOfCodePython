1aeb5df9e1a2dc061edb89ca9c49f115e853fcc5
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2024-12-09 12:12:44 +0000

    day 9 original logic

--- year2024/Day09orig.py
+++ year2024/Day09orig.py
@@ -10,9 +10,9 @@ disk_map = list(map(int, list(data[0])))
 
 disk = []
 free = 0
-for i, x in enumerate(disk_map):
-    disk += [EMPTY if i & 1 else i >> 1] * x
-    free += x if i & 1 else 0
+for pos, x in enumerate(disk_map):
+    disk += [EMPTY if pos & 1 else pos >> 1] * x
+    free += x if pos & 1 else 0
 
 idx_empty = 0
 while free:
@@ -26,17 +26,23 @@ ans1 = sum(i * x for i, x in enumerate(disk))
 print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
 
 disk = []
-for i, x in enumerate(disk_map):
-    disk += [[EMPTY if i & 1 else i >> 1, x]]
-max_id = max(f[0] for f in disk)
+for pos, sz in enumerate(disk_map):
+    if not sz:
+        continue
+    if pos & 1:
+        disk.append([EMPTY, sz])
+    else:
+        disk.append([pos >> 1, sz])
 
-idx_file = len(disk) - 1
+max_id = max(id for id, _ in disk)
+
+fidx = len(disk) - 1
 for id in range(max_id, -1, -1):
-    idx_file = next(i for i in range(idx_file, -1, -1) if disk[i][0] == id)
-    sz = disk[idx_file][1]
-    k = next((i for i in range(0, idx_file) if disk[i][0] == EMPTY and disk[i][1] >= sz), None)
+    fidx = next(i for i in range(fidx, -1, -1) if disk[i][0] == id)
+    sz = disk[fidx][1]
+    k = next((i for i in range(0, fidx) if disk[i][0] == EMPTY and disk[i][1] >= sz), None)
     if k is not None:
-        disk[idx_file][0] = EMPTY
+        disk[fidx][0] = EMPTY
         if sz == disk[k][1]:
             disk[k][0] = id
         else:
@@ -44,10 +50,10 @@ for id in range(max_id, -1, -1):
             disk.insert(k, [id, sz])
 
 ans2 = 0
-i = 0
-for id, cnt in disk:
-    for _ in range(cnt):
-        if id != EMPTY:
-            ans2 += id * i
-        i += 1
+pos = 0
+for id, sz in disk:
+    if id != EMPTY:
+        for p in range(pos, pos + sz):
+            ans2 += id * p
+    pos += sz
 print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
