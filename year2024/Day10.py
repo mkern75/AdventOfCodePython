06ab233a53727369cdc7ece3462c977272313ebc
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2024-12-10 06:32:10 +0000

    day 10 faster

--- year2024/Day10.py
+++ year2024/Day10.py
@@ -5,27 +5,26 @@ INPUT_FILE = "./year2024/data/day10.txt"
 grid = [list(map(int, list(line.rstrip("\n")))) for line in open(INPUT_FILE, "r")]
 R, C = len(grid), len(grid[0])
 
-reachable = [[set() for _ in range(C)] for _ in range(R)]
-cnt = [[0 for _ in range(C)] for _ in range(R)]
+pos = [[] for _ in range(10)]
+for r in range(R):
+    for c in range(C):
+        pos[grid[r][c]].append((r, c))
 
-ans1, ans2 = 0, 0
-for n in range(9, -1, -1):
-    for r in range(R):
-        for c in range(C):
-            if grid[r][c] != n:
-                continue
-            if grid[r][c] == 9:
-                reachable[r][c] = {(r, c)}
-                cnt[r][c] = 1
-            else:
-                for rn, cn in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
-                    if 0 <= rn < R and 0 <= cn < C:
-                        if grid[rn][cn] == grid[r][c] + 1:
-                            reachable[r][c] |= reachable[rn][cn]
-                            cnt[r][c] += cnt[rn][cn]
-            if grid[r][c] == 0:
-                ans1 += len(reachable[r][c])
-                ans2 += cnt[r][c]
+peaks_reachable = [[set() for _ in range(C)] for _ in range(R)]
+n_trails = [[0 for _ in range(C)] for _ in range(R)]
+
+for height in range(9, 0, -1):
+    for r, c in pos[height]:
+        if height == 9:
+            peaks_reachable[r][c] = {(r, c)}
+            n_trails[r][c] = 1
+        for rn, cn in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+            if 0 <= rn < R and 0 <= cn < C and grid[rn][cn] == height - 1:
+                peaks_reachable[rn][cn] |= peaks_reachable[r][c]
+                n_trails[rn][cn] += n_trails[r][c]
+
+ans1 = sum(len(peaks_reachable[r][c]) for r, c in pos[0])
+ans2 = sum(n_trails[r][c] for r, c in pos[0])
 
 print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
 print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
