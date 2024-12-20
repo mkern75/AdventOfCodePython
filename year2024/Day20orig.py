03c1f82c2eac5ca654178f98daf3febddef65b0d
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2024-12-20 10:45:03 +0000

    day 20 slightly shorter version

--- year2024/Day20orig.py
+++ year2024/Day20orig.py
@@ -2,25 +2,41 @@ from time import time
 
 time_start = time()
 
+INF = 1 << 31
+
 INPUT_FILE = "./year2024/data/day20.txt"
 grid = [list(line.rstrip("\n")) for line in open(INPUT_FILE, "r")]
 R, C = len(grid), len(grid[0])
 
-r_start, c_start = next((r, c) for r in range(R) for c in range(C) if grid[r][c] == "S")
-r_end, c_end = next((r, c) for r in range(R) for c in range(C) if grid[r][c] == "E")
 
-INF = 1 << 31
-dist = [[INF] * C for _ in range(R)]
-dist[r_start][c_start] = 0
-bfs = [(r_start, c_start)]
-for r, c in bfs:
-    for rn, cn in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-        if 0 <= r and r < R and 0 <= c and c < C:
-            if grid[rn][cn] != "#" and dist[rn][cn] == INF:
-                dist[rn][cn] = dist[r][c] + 1
-                bfs.append((rn, cn))
+def find(target):
+    for r in range(R):
+        for c in range(C):
+            if grid[r][c] == target:
+                return r, c
+    return -1, -1
 
-normal_time = dist[r_end][c_end]
+
+def calc_dist(r_start, c_start):
+    dist = [[INF] * C for _ in range(R)]
+    dist[r_start][c_start] = 0
+    bfs = [(r_start, c_start)]
+    for r, c in bfs:
+        for rn, cn in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+            if 0 <= r and r < R and 0 <= c and c < C:
+                if grid[rn][cn] != "#" and dist[rn][cn] == INF:
+                    dist[rn][cn] = dist[r][c] + 1
+                    bfs.append((rn, cn))
+    return dist
+
+
+r_start, c_start = find("S")
+dist_start = calc_dist(r_start, c_start)
+
+r_end, c_end = find("E")
+dist_end = calc_dist(r_end, c_end)
+
+normal_time = dist_start[r_end][c_end]
 
 
 def solve(max_cheat, min_saving):
@@ -29,8 +45,6 @@ def solve(max_cheat, min_saving):
         for c in range(C):
             if grid[r][c] == "#":
                 continue
-            if dist[r][c] > normal_time - min_saving:
-                continue
             for rn in range(max(0, r - max_cheat), min(R, r + max_cheat + 1)):
                 dr = abs(r - rn)
                 cheat_remaining = max_cheat - dr
@@ -38,7 +52,8 @@ def solve(max_cheat, min_saving):
                     if grid[rn][cn] == "#":
                         continue
                     dc = abs(c - cn)
-                    cheat_time = dist[r_end][c_end] - (dist[rn][cn] - dist[r][c]) + (dr + dc)
+                    cheat_step = dr + dc
+                    cheat_time = dist_start[r][c] + cheat_step + dist_end[rn][cn]
                     if cheat_time <= normal_time - min_saving:
                         res += 1
     return res
