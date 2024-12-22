88b88814ad7891036a118969f15cc0a82678fb43
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2024-12-22 06:13:23 +0000

    day 24 done

---
+++ year2024/Day22.py
@@ -0,0 +1,48 @@
+from time import time
+
+time_start = time()
+
+INPUT_FILE = "./year2024/data/day22.txt"
+numbers = [int(line.rstrip("\n")) for line in open(INPUT_FILE, "r")]
+
+MOD = 16777216
+N = 2_000
+
+
+def update(x):
+    x = (x ^ (x << 6)) % MOD
+    x = (x ^ (x >> 5)) % MOD
+    x = (x ^ (x << 11)) % MOD
+    return x
+
+
+x = [0] * (N + 1)  # secret numbers
+b = [0] * (N + 1)  # prices
+d = [0] * (N + 1)  # deltas
+seen = [-1] * (1 << 24)
+res = [0] * (1 << 24)
+
+ans1 = 0
+for buyer_id, secret_number in enumerate(numbers):
+    x[0] = secret_number
+    for i in range(1, N + 1):
+        x[i] = update(x[i - 1])
+        b[i] = x[i] % 10
+        d[i] = b[i] - b[i - 1]
+
+    ans1 += x[N]
+
+    for i in range(4, N + 1):
+        # Each change is between -9 and +9.
+        # By adding 9, this range becomes 0 to +18 which fits into 6 bits.
+        # This allows us to map each possible tuple of 4 changes onto a distict index between 0 and 2**24.
+        # This significantly speeds the calculations up (tuples are slow).
+        h = ((d[i - 3] + 9) << 18) | ((d[i - 2] + 9) << 12) | ((d[i - 1] + 9) << 6) | (d[i] + 9)
+        if seen[h] != buyer_id:
+            seen[h] = buyer_id
+            res[h] += b[i]
+
+print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
+
+ans2 = max(res)
+print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
