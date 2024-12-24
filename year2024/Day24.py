a57b93f2edbd5f524f6aa7e65a2d4182aa7ba4e2
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2024-12-24 09:31:37 +0000

    day 24 small refinements + more comments

--- year2024/Day24.py
+++ year2024/Day24.py
@@ -24,52 +24,37 @@ for line in blocks[1]:
     edges[wire1] += [output_wire]
     edges[wire2] += [output_wire]
 
-
-def topological_sort(wires, edges):
-    in_degree = Counter()
-    for wire in wires:
-        for wire_to in edges[wire]:
-            in_degree[wire_to] += 1
-    res = []
-    stack = deque([wire for wire in wires if in_degree[wire] == 0])
-    while stack:
-        wire = stack.popleft()
-        res += [wire]
-        for wire_to in edges[wire]:
-            in_degree[wire_to] -= 1
-            if in_degree[wire_to] == 0:
-                stack.append(wire_to)
-    return res if len(res) == len(wires) else None
-
-
 wires = list(inputs.keys()) + list(gates.keys())
-sorted_wires = topological_sort(wires, edges)
 z_wires = sorted((wire for wire in wires if wire.startswith("z")), reverse=True)
 
+# topological sort of wires
+sorted_wires = []
+in_degree = Counter()
+for wire in wires:
+    for wire_to in edges[wire]:
+        in_degree[wire_to] += 1
+stack = deque([wire for wire in wires if in_degree[wire] == 0])
+while stack:
+    wire = stack.popleft()
+    sorted_wires += [wire]
+    for wire_to in edges[wire]:
+        in_degree[wire_to] -= 1
+        if in_degree[wire_to] == 0:
+            stack.append(wire_to)
+assert len(sorted_wires) == len(wires)
 
-def simulate(inputs, gates, wires_sorted):
-    outputs = {}
-    for wire in wires_sorted:
-        if wire in inputs:
-            outputs[wire] = inputs[wire]
-        else:
-            wire1, op, wire2 = gates[wire]
-            outputs[wire] = OPERATORS[op](outputs[wire1], outputs[wire2])
-    return outputs
-
-
-outputs = simulate(inputs, gates, sorted_wires)
+# simulate
+outputs = {}
+for wire in sorted_wires:
+    if wire in inputs:
+        outputs[wire] = inputs[wire]
+    else:
+        wire1, op, wire2 = gates[wire]
+        outputs[wire] = OPERATORS[op](outputs[wire1], outputs[wire2])
 binary_result = "".join(map(str, [outputs[wire] for wire in z_wires]))
 
 ans1 = int(binary_result, 2)
 print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
-
-
-def find_rule(wire1, op, wire2):
-    for k, (a, b, c) in gates.items():
-        if (wire1, op, wire2) in [(a, b, c), (c, b, a)]:
-            return k
-    return None
 
 # Part 2:
 # For each bit (other than bit 0):
@@ -88,6 +73,13 @@ gate_tmp = [None] * 45
 gate_carry = [None] * 45
 
 swaps = []
+
+
+def find_rule(wire1, operation, wire2):
+    for output_wire, (w1, op, w2) in gates.items():
+        if (wire1, operation, wire2) in [(w1, op, w2), (w2, op, w1)]:
+            return output_wire
+    return None
 
 
 def swap(wire1, wire2):
@@ -116,6 +108,7 @@ for i in range(1, 45):
     check = True
     while check:
         check = False
+
         gate_and[i] = find_rule(x, "AND", y)
 
         gate_xor[i] = find_rule(x, "XOR", y)
@@ -138,8 +131,12 @@ for i in range(1, 45):
             continue
 
         gate_tmp[i] = find_rule(gate_xor[i], "AND", gate_carry[i - 1])
+
         gate_carry[i] = find_rule(gate_tmp[i], "OR", gate_and[i])
+
         # print(f"bit={i}:  and={gate_and[i]}  xor={gate_xor[i]}  z={gate_z[i]}  tmp={gate_tmp[i]}  carry={gate_carry[i]}")
+
+assert len(swaps) == 8
 
 ans2 = ",".join(sorted(swaps))
 print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
