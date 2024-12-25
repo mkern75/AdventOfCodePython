e0f8d73c4abdfc205818dc31677bc370fdf72483
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2024-12-25 05:27:24 +0000

    final update to this year's template

--- year2024/DayTemplate.py
+++ year2024/DayTemplate.py
@@ -1,14 +1,21 @@
 from time import time
-# from collections import defaultdict
+import re
+from collections import defaultdict, Counter
+
+
+def nums(line):
+    return list(map(int, re.findall(r"[-+]?\d+", line)))
+
 
 time_start = time()
-# INPUT_FILE = "./year2024/data/day00test.txt"
-INPUT_FILE = "./year2024/data/day00.txt"
+INPUT_FILE = "./year2024/data/day20test.txt"
+# INPUT_FILE = "./year2024/data/day20.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
 # blocks = [block.splitlines() for block in open(INPUT_FILE, "r").read().split("\n\n")]
 # R, C = len(data), len(data[0])
 # grid = [list(line.rstrip("\n")) for line in open(INPUT_FILE, "r")]
 # grid = defaultdict(lambda: ".", {(r, c): v for r, row in enumerate(data) for c, v in enumerate(row)})
+
 
 ans1, ans2 = 0, 0
 
