0ca0a161f97c1d2d76a4ee5ec025775d0199a9d5
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2025-12-01 16:11:53 +0000

    day 1 shorter

--- year2025/Day01.py
+++ year2025/Day01.py
@@ -1,25 +1,19 @@
 from time import time
 
 time_start = time()
-# INPUT_FILE = "./year2025/data/day01test.txt"
 INPUT_FILE = "./year2025/data/day01.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
 
 ans1, ans2 = 0, 0
 pos, mod = 50, 100
 for line in data:
-    direction, amount = line[0], int(line[1:])
-    ans2 += amount// mod
-    amount %= mod
-    if amount > 0:
-        pos_init = pos
-        if direction == "L":
-            pos -= amount
-        elif direction == "R":
-            pos += amount
-        if pos_init != 0 and pos <= 0 or pos >= mod:
-            ans2 += 1
-        pos %= mod
+    direction, distance = line[0], int(line[1:])
+    ans2 += distance // mod
+    distance %= mod
+    pos_prev, pos = pos, pos + (-distance if direction == "L" else +distance)
+    if pos_prev != 0 and (pos <= 0 or pos >= mod):
+        ans2 += 1
+    pos %= mod
     if pos == 0:
         ans1 += 1
 
