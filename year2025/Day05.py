8d20fbf82fff6dae445e655f60f833385f1891b2
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2025-12-05 05:18:14 +0000

    day 5

---
+++ year2025/Day05.py
@@ -0,0 +1,26 @@
+from time import time
+
+time_start = time()
+INPUT_FILE = "./year2025/data/day05.txt"
+blocks = [block.splitlines() for block in open(INPUT_FILE, "r").read().split("\n\n")]
+
+id_ranges = [tuple(map(int, line.split("-"))) for line in blocks[0]]
+
+ans1 = 0
+for line in blocks[1]:
+    id = int(line)
+    if any(x <= id <= y for x, y in id_ranges):
+        ans1 += 1
+print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
+
+ans2 = 0
+id_ranges.sort()
+lo, hi = id_ranges[0]
+for x, y in id_ranges:
+    if x <= hi <= y:
+        hi = y
+    elif hi < x:
+        ans2 += hi - lo + 1
+        lo, hi = x, y
+ans2 += hi - lo + 1
+print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
