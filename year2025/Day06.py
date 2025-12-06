3def4b5178f3e5a2a75e3c62f01de32d4e5cb0a7
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2025-12-06 05:53:47 +0000

    day 6 single main loop

--- year2025/Day06.py
+++ year2025/Day06.py
@@ -3,7 +3,6 @@ from math import prod
 
 time_start = time()
 
-# INPUT_FILE = "./year2025/data/day06test.txt"
 INPUT_FILE = "./year2025/data/day06.txt"
 data = [line.rstrip("\n") for line in open(INPUT_FILE, "r")]
 
@@ -15,22 +14,19 @@ for i, c in enumerate(data[-1]):
         idx.append(i)
 idx.append(max(len(row) for row in data[:-1]) + 1)
 
-ans1 = 0
+ans1, ans2 = 0, 0
 for i, op in enumerate(ops):
     width = idx[i + 1] - idx[i]
-    nums = [int(data[j][idx[i]:idx[i] + width]) for j in range(n)]
-    ans1 += sum(nums) if op == "+" else prod(nums)
-print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
-
-ans2 = 0
-for i, op in enumerate(ops):
-    width = idx[i + 1] - idx[i]
-    nums = []
+    nums1 = [int(data[j][idx[i]:idx[i] + width]) for j in range(n)]
+    nums2 = []
     for k in range(width - 1):
         s = []
         for j in range(n):
             if idx[i] + k < len(data[j]) and data[j][idx[i] + k] in "0123456789":
                 s.append(data[j][idx[i] + k])
-        nums.append(int("".join(s)))
-    ans2 += sum(nums) if op == "+" else prod(nums)
+        nums2.append(int("".join(s)))
+    ans1 += sum(nums1) if op == "+" else prod(nums1)
+    ans2 += sum(nums2) if op == "+" else prod(nums2)
+
+print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
 print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
